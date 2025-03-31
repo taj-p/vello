1cda9ce4f681ced201412e64cf3f80b2f7986333
Taj Pereira <taj@canva.com>
2025-03-31 12:38:41 +1030

    .

--- sparse_strips/vello_cpu/src/render.rs
+++ sparse_strips/vello_cpu/src/render.rs
@@ -6,7 +6,7 @@
 use crate::fine::Fine;
 use vello_common::coarse::Wide;
 use vello_common::flatten::Line;
-use vello_common::glyph::{DrawGlyphs, GlyphRenderer, GlyphRun, PreparedGlyph};
+use vello_common::glyph::{DrawGlyphs, GlyphRenderer, PreparedGlyph};
 use vello_common::kurbo::{Affine, BezPath, Cap, Join, Rect, Shape, Stroke};
 use vello_common::paint::Paint;
 use vello_common::peniko::Font;
