7cafdbecafe0c437736d13f8a4971136568371e7
Taj Pereira <taj@canva.com>
2025-04-03 10:39:49 +1030

    .

--- sparse_strips/vello_hybrid/shaders/in.hlsl
+++
@@ -1,4 +0,0 @@
-float4 imageMain(uint2 dispatchThreadID, int2 screenSize)
-{
-    return float4(0.3, 0.7, 0.55, 1.0);
-}
--- upstream.sh
+++
@@ -1,3 +0,0 @@
-git fetch grebmeg vello-sparse-strips-hybrid-clean-up
-git merge grebmeg/vello-sparse-strips-hybrid-clean-up
-git push
