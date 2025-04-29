9462448024389b95fa26d70acbd18e4127925807
Taj Pereira <taj@canva.com>
2025-04-30 08:43:47 +0930

    .

--- sparse_strips/vello_hybrid/examples/scenes/src/clip.rs
+++ sparse_strips/vello_hybrid/examples/scenes/src/clip.rs
@@ -65,60 +65,6 @@ pub fn render(ctx: &mut Scene, root_transform: Affine) {
     ctx.set_paint(REBECCA_PURPLE.into());
     ctx.fill_path(&triangle_path);
     ctx.pop_layer();
-
-    //let clip_rect = Rect::new(0.0, 0.0, 400.0, 400.0);
-
-    //let stroke = Stroke::new(10.0);
-    //ctx.set_paint(CORNFLOWER_BLUE.into());
-    //ctx.set_stroke(stroke);
-    //ctx.stroke_rect(&clip_rect);
-
-    //ctx.push_clip_layer(&clip_rect.to_path(0.1));
-
-    //// Make a checkerboard pattern of 10x10 squares
-    ////ctx.set_paint(RED.into());
-    ////ctx.fill_rect(&Rect::new(0.0, 0.0, 300.0, 100.0));
-
-    //let mut red = false;
-    ////for i in 0..100 {
-    ////    println!("i: {}", i);
-    ////    if red {
-    ////        ctx.set_paint(RED.into());
-    ////    } else {
-    ////        ctx.set_paint(BLUE.into());
-    ////    }
-    ////    ctx.fill_rect(&Rect::new(
-    ////        0.0,
-    ////        i as f64 * 10.0,
-    ////        100.0,
-    ////        i as f64 * 10.0 + 100.0,
-    ////    ));
-    ////    red = !red;
-    ////}
-
-    //ctx.set_paint(REBECCA_PURPLE.into());
-    //ctx.fill_rect(&Rect::new(10.0, 10.0, 500.0, 500.0));
-    //ctx.pop_layer();
-
-    //// ctx.set_transform(Affine::translate(Vec2::new(100.0, 0.0)));
-
-    ////let mut triangle_path = BezPath::new();
-    ////triangle_path.move_to((10.0, 10.0));
-    ////triangle_path.line_to((90.0, 20.0));
-    ////triangle_path.line_to((20.0, 90.0));
-    ////triangle_path.close_path();
-
-    ////let stroke = Stroke::new(3.0);
-    ////ctx.set_paint(DARK_BLUE.into());
-    ////ctx.set_stroke(stroke);
-    ////ctx.stroke_path(&triangle_path);
-
-    ////let star_path = circular_star(Point::new(50., 50.), 13, 25., 45.);
-
-    ////ctx.push_clip_layer(&star_path);
-    ////ctx.set_paint(REBECCA_PURPLE.into());
-    ////ctx.fill_path(&triangle_path);
-    ////ctx.pop_layer();
 }
 
 pub(crate) fn circular_star(center: Point, n: usize, inner: f64, outer: f64) -> BezPath {
